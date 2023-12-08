56e6e69d2bb6099b42f40e0aef00aa53b9c70731
BSJJC <805086021@qq.com>
2023-12-08 20:18:27 +0800

    chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,vue}'],
+  content: ['./public/**/*.html', './src/**/*.{js,ts,vue}'],
   theme: {
     extend: {},
   },
