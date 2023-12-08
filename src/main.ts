ecb78cc0d119f0a7fd4d40c4432daf9d5aab1429
BSJJC <805086021@qq.com>
2023-12-08 20:38:56 +0800

    refa

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { createApp } from 'vue';
-import './index.css';
-import './style.css';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
 import App from './App.vue';
+
+import './css/index.css';
+import './css/style.css';
+import './css/fonts.css';
 
 const app = createApp(App);
 
-app.use(ElementPlus);
 app.mount('#app');
