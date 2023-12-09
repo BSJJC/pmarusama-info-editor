5516d3f250ada8911c7c5d382662dfc1fb520e56
BSJJC <805086021@qq.com>
2023-12-09 22:40:54 +0800

    feat: info-p

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex flex-row">
+  <div class="w-full h-screen flex flex-row">
     <WorkSpaceIndex></WorkSpaceIndex>
 
     <PreviewIndex></PreviewIndex>
--- src/view/work-space/components/info-p.vue
+++ src/view/work-space/components/info-p.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
-    <el-input
-      type="textarea"
-      class="w-[300px]"
-      v-for="i in Math.max(props.data!.text.length - 1, 1)"
-      :key="i"
-      v-model="(form.data.components[props.componentId!].data as TData).text[i - 1]"
-    />
+  <div class="flex flex-col space-y-4">
+    <div v-for="i in (form.data.components[props.componentId!].data as TData).text.length" :key="i">
+      <el-input
+        v-if="(form.data.components[props.componentId!].data as TData).text[i - 1] !== null"
+        type="textarea"
+        class="w-[300px]"
+        v-model="(form.data.components[props.componentId!].data as TData).text[i - 1]"
+      />
+
+      <el-text v-else class="mx-1 text-center" type="warning" size="large" tag="b">
+        an empty line here
+      </el-text>
+    </div>
+
+    <div>
+      <el-button @click="addNewLine">start a new line</el-button>
+      <el-button @click="addEmptyLine">add an empty line</el-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useForm } from '@/store/useForm';
-
-const { form } = useForm();
 
 type TData = {
   text: Array<string | null>;
@@ -24,7 +32,24 @@ const props = defineProps({
   data: Object,
 });
 
-(form.data.components[props.componentId!].data as TData).text = [];
+const { form } = useForm();
+
+// initial text array
+(form.data.components[props.componentId!].data as TData).text = [''];
+
+/**
+ * add a new string line to text array.
+ */
+function addNewLine(): void {
+  (form.data.components[props.componentId!].data as TData).text.push('');
+}
+
+/**
+ * add a null to text array, which means an empty line.
+ */
+function addEmptyLine(): void {
+  (form.data.components[props.componentId!].data as TData).text.push(null);
+}
 </script>
 
 <style></style>
