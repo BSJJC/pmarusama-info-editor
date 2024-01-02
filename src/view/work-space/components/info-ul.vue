9e360a4597c32709ef768775418abdccad30fda0
BSJJC <805086021@qq.com>
2024-01-02 23:42:20 +0800

    chore

--- src/view/work-space/components/info-ul.vue
+++ src/view/work-space/components/info-ul.vue
@@ -1,42 +1,68 @@
 <template>
   <div>
-    <el-tree
+    <div
       v-for="(data, index) in dataSources"
       :key="index"
-      :data="data"
-      node-key="id"
-      default-expand-all
-      :expand-on-click-node="false"
-      class="rounded-lg opacity-50"
+      class="flex justify-between items-center"
     >
-      <template #default="{ node, data }">
-        <span class="flex flex-1 justify-between items-center pr-[8px] text-[14px]">
-          <span>{{ node.label }}</span>
-          <span>
-            <a @click="append(data, index)">Append</a>
-            <a style="margin-left: 8px" @click="remove(node, data, index)">Delete</a>
+      <el-tree
+        :data="data"
+        node-key="id"
+        default-expand-all
+        :expand-on-click-node="false"
+        class="rounded-lg w-[80%]"
+      >
+        <template #default="{ node, data }">
+          <span class="flex flex-1 justify-between items-center pr-[8px] text-[14px]">
+            <span>{{ node.label }}</span>
+            <span>
+              <a @click="append(data, index)">Append</a>
+              <a style="margin-left: 8px" @click="remove(node, data, index)">Delete</a>
+            </span>
           </span>
-        </span>
-      </template>
-    </el-tree>
+        </template>
+      </el-tree>
 
-    <el-button @click="t">123</el-button>
+      <el-button type="danger" plain @click="showDeleteTree(index)">delete</el-button>
+    </div>
+
+    <!-- 
+      delete tree dialog
+     -->
+    <el-dialog
+      v-model="setDeleteTree"
+      :show-close="false"
+      draggable
+      align-center
+      class="rounded-lg"
+    >
+      <el-tree
+        :data="deleteTreeData"
+        node-key="id"
+        default-expand-all
+        :expand-on-click-node="false"
+        class="rounded-lg"
+      />
+    </el-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, Ref } from 'vue';
 import type Node from 'element-plus/es/components/tree/src/model/node';
 
 type TTree = {
   id: number;
   label: string;
-  children?: TTree[];
+  children?: Array<TTree>;
 };
 
 let id = 0;
 
 const dataSources = ref<Array<Array<TTree>>>([[]]);
+
+const setDeleteTree: Ref<boolean> = ref(false);
+const deleteTreeData = ref<Array<TTree>>([]);
 
 function append(data: TTree, index: number): void {
   const newChild = { id: id++, label: 'testtest', children: [] };
@@ -54,13 +80,10 @@ function remove(node: Node, data: TTree, index: number): void {
   dataSources.value[index] = [...dataSources.value[index]];
 }
 
-const t = () => {
-  if (!dataSources.value[0]) dataSources.value[0] = [];
-  dataSources.value[0].push({
-    id,
-    label: '121',
-  });
-};
+function showDeleteTree(index: number): void {
+  deleteTreeData.value = dataSources.value[index];
+  setDeleteTree.value = true;
+}
 </script>
 
 <style></style>
