d3df2170174c1ca771f38eed22272a39923a7f71
BSJJC <805086021@qq.com>
2024-02-03 16:43:45 +0800

    chore

---
+++ src/view/preview/components/preview-p.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p v-for="(i, index) in texts" :key="index">
+      {{ i ? i : ` ` }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+type TData = {
+  texts: Array<string | null>;
+};
+
+const props = defineProps({
+  data: Object as () => TData,
+});
+
+const texts = props.data?.texts;
+</script>
+
+<style scoped>
+p {
+  line-height: 1.8;
+  color: #787878;
+  margin-bottom: 20px;
+}
+</style>
--- src/view/work-space/components/info-p.vue
+++ src/view/work-space/components/info-p.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="flex flex-col space-y-4">
-    <div v-for="(i, index) in text.length" :key="i" class="flex justify-between items-center">
-      <el-input v-if="text[i - 1] !== null" type="textarea" class="w-[80%]" v-model="text[index]" />
+    <div v-for="(i, index) in texts.length" :key="i" class="flex justify-between items-center">
+      <el-input
+        v-if="texts[i - 1] !== null"
+        type="textarea"
+        class="w-[80%]"
+        v-model="texts[index]"
+      />
 
       <div v-else class="mx-1 text-[#e6a23c] text-[1rem]">
         <b>an empty line here</b>
       </div>
 
-      <el-button :disabled="text.length === 1" type="danger" plain @click="deleteLine(i - 1)">
+      <el-button :disabled="texts.length === 1" type="danger" plain @click="deleteLine(i - 1)">
         delete
       </el-button>
     </div>
@@ -29,24 +34,25 @@ const props = defineProps({
 
 const { form } = useForm();
 
-const text: Ref<Array<string | null>> = ref(['']);
+const texts: Ref<Array<string | null>> = ref(['']);
 
 function addNewLine(): void {
-  text.value.push('');
+  texts.value.push('');
 }
-
 function addEmptyLine(): void {
-  text.value.push(null);
+  texts.value.push(null);
 }
 
 function deleteLine(index: number) {
-  text.value.splice(index, 1);
+  texts.value.splice(index, 1);
 }
 
 watch(
-  () => text.value,
+  () => texts.value,
   () => {
-    form.data.components[props.componentId!].data = text.value;
+    form.data.components[props.componentId!].data = {
+      texts: texts.value,
+    };
   },
   {
     immediate: true,
