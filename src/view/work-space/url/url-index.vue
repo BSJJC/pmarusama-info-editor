27266d9d028e6c51ad86d395e6f42bebdf12eee1
BSJJC <805086021@qq.com>
2024-02-23 00:15:15 +0800

    feat: url

--- src/view/work-space/url/url-index.vue
+++ src/view/work-space/url/url-index.vue
@@ -1,37 +1,36 @@
 <template>
-  <div class="w-full h-full py-10 px-5 flex flex-col justify-center items-center">
-    <div class="w-full flex space-x-4 mb-4">
-      <span>url:</span>
-      <el-input v-model="_url" @change="completeInput" />
-    </div>
+  <div class="w-full h-full py-10 px-5">
+    <el-scrollbar height="85vh" class="w-full">
+      <div class="flex flex-col justify-center items-center w-full space-y-10">
+        <div class="flex w-1/2 justify-center items-center space-x-4">
+          <span>url:</span>
+          <el-input v-model="url" />
+        </div>
 
-    <TransitionGroup>
-      <div
-        v-if="legalURL === false"
-        class="w-full h-full rounded-lg flex justify-center items-center"
-      >
-        <!-- <lottieAniamtion :data="animationData" class="w-1/2"></lottieAniamtion> -->
+        <link-prevue :url="url"></link-prevue>
+
+        <el-button
+          v-show="urlPattern.test(url)"
+          type="primary"
+          plain
+          round
+          class="w-full h-[5vh] text-lg"
+        >
+          submit
+        </el-button>
       </div>
-    </TransitionGroup>
+    </el-scrollbar>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, Ref, onBeforeMount } from 'vue';
-import lottieAniamtion from '@/components/lottie-aniamtion.vue';
+import { ref, Ref } from 'vue';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import LinkPrevue from 'link-prevue';
 
-const _url: Ref<string> = ref('');
 const url: Ref<string> = ref('');
-
-const legalURL: Ref<boolean> = ref(false);
-
-const animationData: Ref<object | undefined> = ref<object>();
-
-async function completeInput(): Promise<void> {}
-
-onBeforeMount(async () => {
-  animationData.value = (await import('@/assets/lottieAnimations/404.json')).default;
-});
+const urlPattern: RegExp = /^(https?:\/\/)?([\w.]+)\.([a-z]{2,})(\/\S*)?$/i;
 </script>
 
 <style></style>
