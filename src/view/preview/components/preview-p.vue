4566321bda0d6e5bfc6c5777c0b62f839553d679
BSJJC <805086021@qq.com>
2024-03-24 19:28:23 +0800

    feat: add a tag

--- src/view/preview/components/preview-p.vue
+++ src/view/preview/components/preview-p.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    <p v-for="(i, index) in texts" :key="index" v-html="i || '<br/>'"></p>
+    <div v-for="(msgs, msgIndex) in texts" :key="msgIndex">
+      <div v-for="(msg, msgIndex) in msgs" :key="msgIndex">
+        <div v-if="msg.type === 'string'" v-html="msg.msg"></div>
+
+        <a v-else-if="msg.type === 'hyperlink'" :href="msg.msg.link" target="_blank">
+          {{ msg.msg.text }}
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+type TstringMsg = {
+  type: 'string';
+  msg: string;
+};
+
+type ThyperlinkMsg = {
+  type: 'hyperlink';
+  msg: {
+    text: string;
+    link: string;
+  };
+};
+
+type TtextMsg = Array<TstringMsg | ThyperlinkMsg>;
+
 type TData = {
-  texts: Array<string | null>;
+  texts: Array<TtextMsg | null>;
 };
 
 const props = defineProps({
--- src/view/work-space/form/components/info-p.vue
+++ src/view/work-space/form/components/info-p.vue
@@ -1,19 +1,93 @@
 <template>
   <div class="flex flex-col space-y-4">
-    <div v-for="(i, index) in texts.length" :key="i" class="flex justify-between items-center">
-      <el-input v-if="texts[i - 1] !== null" type="textarea" v-model="texts[index]" />
-
-      <div v-else class="mx-1 text-[#e6a23c] text-[1rem]">
+    <div
+      v-for="(msgs, index) in texts"
+      :key="index"
+      class="flex justify-between items-center p-2 bg-[#a6a9ad20] rounded-lg"
+    >
+      <!-- empty line -->
+      <div v-if="msgs === null" class="mx-1 text-[#e6a23c] text-[1rem]">
         <b>an empty line here</b>
       </div>
 
-      <el-button :disabled="texts.length === 1" type="danger" plain @click="deleteLine(i - 1)">
+      <!-- not empty line -->
+      <div v-else class="w-[80%] flex flex-col gap-4">
+        <div v-for="(msg, msgIndex) in msgs" :key="msgIndex">
+          <!-- string msg -->
+          <el-input v-if="msg.type === 'string'" type="textarea" v-model="msg.msg" />
+
+          <!-- hyperlink msg -->
+          <div
+            v-else-if="(msg.type = 'hyperlink')"
+            class="flex flex-col text-base text-[#909399] gap-y-2"
+          >
+            <div class="flex flex-row gap-x-2">
+              <span>text:</span>
+              <el-input v-model="msg.msg.text" />
+            </div>
+
+            <div class="flex flex-row gap-x-2">
+              <span>link:</span>
+              <el-input v-model="msg.msg.link" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <el-button :disabled="texts.length === 1" type="danger" plain @click="deleteLine(index)">
         delete
       </el-button>
     </div>
 
-    <div>
+    <div class="flex justify-between items-center">
       <el-button @click="addNewLine" type="primary" plain>start a new line</el-button>
+
+      <div>
+        <el-popover placement="top-start" wdith="350" trigger="hover">
+          <template #reference>
+            <el-button
+              @click="connectMsg('hyperlink')"
+              type="primary"
+              plain
+              :disabled="addEmptyLineActive()"
+            >
+              connect hyperlink
+            </el-button>
+          </template>
+
+          <template #default>
+            <span v-if="!addEmptyLineActive()">
+              this operation does not create a new line, but as part of the previous line of an a
+              tag.
+            </span>
+
+            <span v-else>a tag can not be connected to a null line.</span>
+          </template>
+        </el-popover>
+
+        <el-popover placement="top-start" wdith="350" trigger="hover">
+          <template #reference>
+            <el-button
+              @click="connectMsg('string')"
+              type="primary"
+              plain
+              :disabled="addEmptyLineActive()"
+            >
+              connect string
+            </el-button>
+          </template>
+
+          <template #default>
+            <span v-if="!addEmptyLineActive()">
+              this operation does not create a new line, but as part of the previous line of an a
+              tag.
+            </span>
+
+            <span v-else>can not be connected to a null line.</span>
+          </template>
+        </el-popover>
+      </div>
+
       <el-button @click="addEmptyLine" type="warning" plain>add an empty line</el-button>
     </div>
   </div>
@@ -29,11 +103,56 @@ const props = defineProps({
 
 const { form } = useForm();
 
-const texts: Ref<Array<string | null>> = ref(['']);
+type TstringMsg = {
+  type: 'string';
+  msg: string;
+};
+
+type ThyperlinkMsg = {
+  type: 'hyperlink';
+  msg: {
+    text: string;
+    link: string;
+  };
+};
+
+type TtextMsg = Array<TstringMsg | ThyperlinkMsg>;
+
+const texts: Ref<Array<TtextMsg | null>> = ref([[{ type: 'string', msg: '' }]]);
 
 function addNewLine(): void {
-  texts.value.push('');
+  texts.value.push([
+    {
+      type: 'string',
+      msg: '',
+    },
+  ]);
 }
+
+function connectMsg(type: 'string' | 'hyperlink'): void {
+  const msgIndex = texts.value.length - 1;
+
+  if (type === 'hyperlink') {
+    texts.value[msgIndex]!.push({
+      type: 'hyperlink',
+      msg: {
+        text: '',
+        link: '',
+      },
+    });
+  } else if (type === 'string') {
+    texts.value[msgIndex]!.push({
+      type: 'string',
+      msg: '',
+    });
+  }
+}
+
+function addEmptyLineActive(): boolean {
+  if (texts.value[texts.value.length - 1]) return false;
+  else return true;
+}
+
 function addEmptyLine(): void {
   texts.value.push(null);
 }
@@ -56,8 +175,4 @@ watch(
 );
 </script>
 
-<style scoped>
-.el-textarea {
-  width: 80%;
-}
-</style>
+<style></style>
