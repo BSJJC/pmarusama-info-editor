49772fea544500cdacad2c86de336a2f83e19506
BSJJC <805086021@qq.com>
2024-04-13 21:40:18 +0800

    chore

--- src/view/work-space/form/components/info-table-test.vue
+++ src/view/work-space/form/components/info-table-test.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>table test</div>
+  <div>
+    <div class="flex flex-col space-y-2">
+      <div
+        v-for="rows in maxRows"
+        :key="rows"
+        class="w-full h-[40px] flex flex-row justify-start items-center space-x-4"
+      >
+        <div
+          v-for="cols in maxCols"
+          :key="cols"
+          class="w-[50px] h-[40px] rounded-lg bg-[#909399] transition-all ease-in-out duration-100 hover:cursor-pointer hover:-translate-y-1"
+        ></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -12,6 +26,9 @@ import {
 type TTable = Array<Array<Array<string | null>>>;
 
 const table: TTable = [];
+
+const maxRows = 8;
+const maxCols = 8;
 </script>
 
 <style></style>
